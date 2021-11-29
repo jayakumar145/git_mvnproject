8564f033c076e06216845277e0259efd4316fc76
jayakumar145 <95216765+jayakumar145@users.noreply.github.com>
2021-11-29 17:32:24 +0530

    Update App.java
    Git communication

--- DemoProject/src/main/java/com/abc/App.java
+++ DemoProject/src/main/java/com/abc/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! ...  Jenkins ...Testing ....!" );
+        System.out.println( "TRIGGER From Git To Jenkins" );
     }
 }
